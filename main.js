3ad4f5053ce12213307c4b0109802e0cf5b877ab
giordanoale <a.giordano89@gmail.com>
2021-03-12 18:15:26 +0100

    Aggiunta navigazione con i tasti

--- main.js
+++ main.js
@@ -10,44 +10,66 @@ $(function() {
 
 var btnPrev = $(".prev");
 var btnNext = $(".next");
+var navBullet = $(".fa-circle");
 
-btnPrev.click(
-  function(){
-    var imgActive = $(".images img.active");
-    imgActive.removeClass("active");
-      if (imgActive.hasClass("first") == true) {
-        $(".images img.last").addClass("active");
-      } else {
-        imgActive.prev().addClass("active");
-        }
-    var bltActive = $(".nav i.active");
-    bltActive.removeClass("active");
-      if (bltActive.hasClass("first") == true) {
-        $(".nav i.last").addClass("active");
-      } else {
-        bltActive.prev().addClass("active");
+//Questa funzione serve a passare all'immagine precedente
+function prevImage() {
+  var imgActive = $(".images img.active");
+  imgActive.removeClass("active");
+    if (imgActive.hasClass("first") == true) {
+      $(".images img.last").addClass("active");
+    } else {
+      imgActive.prev().addClass("active");
       }
+  var bltActive = $(".nav i.active");
+  bltActive.removeClass("active");
+    if (bltActive.hasClass("first") == true) {
+      $(".nav i.last").addClass("active");
+    } else {
+      bltActive.prev().addClass("active");
     }
+}
 
-);
-
-btnNext.click(
-  function(){
-    var imgActive = $(".images img.active");
-    imgActive.removeClass("active");
-      if (imgActive.hasClass("last") == true) {
-        $(".images img.first").addClass("active");
-      } else {
-        imgActive.next().addClass("active");
-        }
-    var bltActive = $(".nav i.active");
-    bltActive.removeClass("active");
-      if (bltActive.hasClass("last") == true) {
-        $(".nav i.first").addClass("active");
-      } else {
-        bltActive.next().addClass("active");
+//Questa funzione serve a passare all'immagine successiva
+function nextImage(){
+  var imgActive = $(".images img.active");
+  imgActive.removeClass("active");
+    if (imgActive.hasClass("last") == true) {
+      $(".images img.first").addClass("active");
+    } else {
+      imgActive.next().addClass("active");
       }
+  var bltActive = $(".nav i.active");
+  bltActive.removeClass("active");
+    if (bltActive.hasClass("last") == true) {
+      $(".nav i.first").addClass("active");
+    } else {
+      bltActive.next().addClass("active");
+    }
+}
+
+btnPrev.click(prevImage);
+btnNext.click(nextImage);
+
+$(document).keydown(
+  function(e) {
+    if(e.keyCode == 39) {
+        nextImage();
+    } else if (e.keyCode == 37) {
+        prevImage();
+    }
   }
-);
+)
+
+// navBullet.click(
+//   function() {
+//     var bltActive = $(".nav i.active");
+//     bltActive.removeClass("active");
+//     $(this).addClass("active");
+//     var imgActive = $(".images img.active");
+//     imgActive.removeClass("active");
+//
+//   }
+// );
 
 });
